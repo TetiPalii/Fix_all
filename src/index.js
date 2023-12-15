8daa8383b7a94ac6bf0137884c0c8f32ba444105
TetiPalii <tpalii1945@gmail.com>
2023-12-15 11:57:10 +0100

    reset styles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import './fonts.css';
+import 'styles/fonts.css';
+import 'styles/variables.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
